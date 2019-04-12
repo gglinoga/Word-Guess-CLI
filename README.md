97e03b2e649e22f2022b79a221170db04f25eac8
Greg Glinoga <gglinoga@gmail.com>
2019-04-11 21:49:45 -0700

    markdown, code cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Word-Guess-CLI
+
+This is a Word Guess game utilizing a command line interface.  Additionally, the game utilizes the inquirer npm package and constructor functions.
+
+In order to play, the user must enter 'npm install' in the terminal in order to install the dependencies.  After installing the required packages, open index.js.  The player is prompted to guess a letter from a randomly chosen word.  There are limited number of guesses before losing the game.  To win, the player must guess all the characters in the word.
--- Word.js
+++ Word.js
@@ -1,41 +1,27 @@
 var Letter = require('./Letter.js');
 
 function Word(guessWord) {
-    this.str = guessWord;
-    this.gwArr = [];
-    this.displayWord = [];
-    for (var i = 0; i < guessWord.length; i++) {
-        var guess = guessWord.charAt(i);
-        var newLetter = new Letter(guess);
-        this.gwArr.push(newLetter); //array of new letter obj for the word to be guessed (guessWord)
-        // console.log(this.gwArr)
-    }
-    this.display = function () {
-        this.displayWord = [];
-        for (var i = 0; i < this.gwArr.length; i++) {
-            var show = this.gwArr[i].guessSwitch();
-            if (show) {
-                // console.log(i, show);
-                this.displayWord.push(show);
-            }
-        }
-
-
-
-        //return string representing word, chars or underscore, concatenate
-        // console.log(this.displayWord.join(" "));
-    };
-    this.checkGuess = function (guess) {
-        //takes character as argument, calls letter.guessCheck on each letter obj.  forEach???
-        for (var i = 0; i < this.gwArr.length; i++) {
-            this.gwArr[i].guessCheck(guess);
-            // console.log(this.gwArr[i])
-        }
-    }
+	this.str = guessWord;
+	this.gwArr = [];
+	this.displayWord = [];
+	for (var i = 0; i < guessWord.length; i++) {
+		var guess = guessWord.charAt(i);
+		var newLetter = new Letter(guess);
+		this.gwArr.push(newLetter);
+	}
+	this.display = function() {
+		this.displayWord = [];
+		for (var i = 0; i < this.gwArr.length; i++) {
+			var show = this.gwArr[i].guessSwitch();
+			if (show) {
+				this.displayWord.push(show);
+			}
+		}
+	};
+	this.checkGuess = function(guess) {
+		for (var i = 0; i < this.gwArr.length; i++) {
+			this.gwArr[i].guessCheck(guess);
+		}
+	}
 }
-
 module.exports = Word;
-
-// var test = new Word("test");
-// test.checkGuess("t");
-// console.log(test.display());
